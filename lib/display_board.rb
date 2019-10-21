6ddd21de79a4acefd3d0e2439f963aef7f35b669
communal-ninja-2078 <fbuzeta11@gmail.com>
2019-10-21 15:08:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,11 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["o", "x", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts "   |   |   "
+  puts "  #{board[0]} | #{board[1]}  | #{board[2]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts "  #{board[3]} | #{board[4]}  | #{board[5]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 end
 
 display_board(board)
-puts "x#{board[1]}"
